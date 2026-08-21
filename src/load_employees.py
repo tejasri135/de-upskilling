9648d4f97a7db5f9f6afb747381975dbcb76928a
Tejasri <tejasrisunku@gmail.com>
2026-08-21 14:05:42 +0530

    Load a csv to postgres table

--- src/load_employees.py
+++ src/load_employees.py
@@ -9,7 +9,6 @@ conn = psycopg2.connect(
     password="devpassword"
 )
 cur = conn.cursor()
-
 cur.execute("""
     CREATE TABLE IF NOT EXISTS employees (
         employee_id INT PRIMARY KEY,
@@ -17,19 +16,26 @@ cur.execute("""
         department TEXT
     )
 """)
+cur.execute("truncate table employees;")
 
-# Load employee data from the CSV into the database
-with open("data/employees.csv", newline="") as f:
-    reader = csv.DictReader(f)
+# open data/employees.csv
+
+with open("data/employees.csv","r",newline="") as f:
+    reader = csv.reader(f)
+    next(reader)
     for row in reader:
-        cur.execute(
-            """
-            INSERT INTO employees (employee_id, name, department)
-            VALUES (%s, %s, %s)
-            ON CONFLICT (employee_id) DO NOTHING
-            """,
-            (int(row["employee_id"]), row["name"], row["department"]),
-        )
+        cur.execute("INSERT INTO employees VALUES (%s, %s, %s)", row)
+        ## upsert is other option instead of truncate. 
+        # upsert (ON CONFLICT DO UPDATE) is the alternative to truncate.
+        # truncate = full refresh, handles deletes, rewrites everything.
+        # upsert = incremental, cheaper on large tables, won't remove deleted rows.
+#       cur.execute("INSERT INTO employees (employee_id, name, department)
+#       VALUES (%s, %s, %s)
+#       ON CONFLICT (employee_id) DO UPDATE
+#       SET name = EXCLUDED.name,  
+#       department = EXCLUDED.department 
+#       """, row)
+     
 
 conn.commit()
 cur.close()
